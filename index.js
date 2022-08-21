f7aa5c48b09903e71247c7d56c69aa0da8c87123
Lucas Santos <lucassnts963@gmail.com>
2022-08-21 02:16:45 -0300

    configurando o projeto

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('entry point...')
+import express from 'express'
+
+const HOST = 'localhost'
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.status(200).send('YouTube Download Service')
+})
+
+app.listen(PORT, () => console.log(`Servidor rodando em ${HOST}:${PORT}`))
